908d0fe2c376839421b58a4429a2833c2ca15be7
Andreas Muehlmann <andi.muehlmann@arcor.de>
2024-08-11 20:23:12 +0200

    use api to get packages and get download links

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://apps.microsoft.com/detail/9nblggh42ths?hl=de-de&gl=DE"
-    request = requests.get(url)
+    uri = 'https://store.rg-adguard.net/api/GetFiles'
+    data = {
+        'type': 'url',
+        'url': 'https://apps.microsoft.com/detail/9nblggh42ths?hl=de-de&gl=DE',
+        'ring': 'Retail'
+    }
 
-    soup = BeautifulSoup(request.content, 'html.parser')
-    print(soup.prettify())
+    response = requests.post(
+            uri,
+            data=data,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            )
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    a_tags = soup.find_all('a')
+    packages = filter(lambda a_tag:
+                      a_tag.text.endswith('.appx')
+                      or a_tag.text.endswith('.appxbundle')
+                      or a_tag.text.endswith('.msix')
+                      or a_tag.text.endswith('.msixbundle'), a_tags)
+    packages = filter(lambda package: '_neutral_' in package.text, packages)
+
+    for package in packages:
+        print(package['href'])
 
 
 if __name__ == "__main__":
